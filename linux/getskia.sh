75a4c17dc6df0186232b325898c46116730ac11e
Nikita Tsukanov <keks9n@gmail.com>
2015-11-08 01:09:33 +0300

    Added patch & android build to skia script

--- linux/getskia.sh
+++ linux/getskia.sh
@@ -7,6 +7,14 @@ cd skia
 tools/install_dependencies.sh
 echo Running sync-and-gyp, this will take a while
 export GYP_DEFINES=skia_shared_lib=1
+git checkout gyp/common.gypi gyp/common_conditions.gypi gyp/common_variables.gypi
+patch -p1 < ../libperspesk/linux/skia.patch
 bin/sync-and-gyp
 echo Running ninja
 ninja -C out/Release
+
+set +e
+for arch in `echo arm_v7 arm64 x86 mips`
+do
+BUILDTYPE=Release ./platform_tools/android/bin/android_ninja -d $arch
+done
