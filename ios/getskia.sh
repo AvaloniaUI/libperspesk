21b799dfcc762aeb2bd510551657b4d785bf0cac
Nikita Tsukanov <keks9n@gmail.com>
2015-11-21 23:19:26 +0300

    Now using own skia fork

--- ios/getskia.sh
+++ ios/getskia.sh
@@ -1,10 +1,10 @@
 source ./env
 cd ../..
 git clone 'https://chromium.googlesource.com/chromium/tools/depot_tools.git'
-git clone 'https://skia.googlesource.com/skia
+git clone 'https://github.com/Perspex/skia
 set -e
 cd skia
-git checkout 844a0b4
+git checkout perspex
 echo Running sync-and-gyp, this will take a while
 export GYP_DEFINES="skia_os='ios' skia_arch_type='arm' armv7=1 arm_neon=0"
 
